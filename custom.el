4db22688bc7681a34a9dc220323dfd18b657db2c
Tomke Pfoch <pfoch.tomke@gmail.com>
2025-06-26 00:48:26 +0200

    Font Freeze

--- custom.el
+++ custom.el
@@ -9,15 +9,5 @@
      "04aa1c3ccaee1cc2b93b246c6fbcd597f7e6832a97aaeac7e5891e6863236f9f"
      "6bdc4e5f585bb4a500ea38f563ecf126570b9ab3be0598bdf607034bb07a8875"
      default))
- 
- '(org-babel-load-languages
-   '((awk . t) (latex . t) (dot . t) (plantuml . t) (shell . t)
-     (haskell . t) (C . t) (emacs-lisp . t)))
  '(org-latex-preview-cache 'temp)
  '(org-safe-remote-resources '("\\`https://fniessen\\.github\\.io\\(?:/\\|\\'\\)")))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(olivetti-fringe ((t (:inherit fringe :background "#ffffff")))))
