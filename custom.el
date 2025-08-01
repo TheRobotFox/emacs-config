33be08ab1def1ec3bf3e602e76ee15c21d5d3b8e
Tomke Pfoch <pfoch.tomke@gmail.com>
2025-08-01 12:05:33 +0200

    eglot

--- custom.el
+++ custom.el
@@ -10,8 +10,10 @@
      "04aa1c3ccaee1cc2b93b246c6fbcd597f7e6832a97aaeac7e5891e6863236f9f"
      "6bdc4e5f585bb4a500ea38f563ecf126570b9ab3be0598bdf607034bb07a8875"
      default))
+ '(org-confirm-babel-evaluate nil)
  '(org-latex-preview-cache 'temp)
- '(org-safe-remote-resources '("\\`https://fniessen\\.github\\.io\\(?:/\\|\\'\\)")))
+ '(org-safe-remote-resources '("\\`https://fniessen\\.github\\.io\\(?:/\\|\\'\\)"))
+ '(org-startup-with-link-previews t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
