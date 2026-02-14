bf3f802c6a495284fde527c5352e517a56948966
Tomke Pfoch <pfoch.tomke@gmail.com>
2026-02-14 09:39:27 +0100

    c

--- custom.el
+++ custom.el
@@ -8,15 +8,14 @@
    '("0dd2666921bd4c651c7f8a724b3416e95228a13fca1aa27dc0022f4e023bf197"
      "b11edd2e0f97a0a7d5e66a9b82091b44431401ac394478beb44389cf54e6db28"
      "04aa1c3ccaee1cc2b93b246c6fbcd597f7e6832a97aaeac7e5891e6863236f9f"
-     "6bdc4e5f585bb4a500ea38f563ecf126570b9ab3be0598bdf607034bb07a8875"
-     default))
+     "6bdc4e5f585bb4a500ea38f563ecf126570b9ab3be0598bdf607034bb07a8875" default))
  '(org-confirm-babel-evaluate nil)
+ '(org-latex-packages-alist nil)
  '(org-latex-preview-cache 'temp)
  '(org-safe-remote-resources '("\\`https://fniessen\\.github\\.io\\(?:/\\|\\'\\)"))
  '(org-startup-with-link-previews t)
  '(package-vc-selected-packages
-   '((doxymacs :url "https://github.com/pniedzielski/doxymacs.git"
-	       :lisp-dir "lisp/"))))
+   '((doxymacs :url "https://github.com/pniedzielski/doxymacs.git" :lisp-dir "lisp/"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
