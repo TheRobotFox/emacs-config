2ff5d216ce96b51f752a1f963183128dc5998d0b
Tomke <pfoch.tomke@gmail.com>
2026-04-16 22:53:54 +0200

    cleanup

--- custom.el
+++ custom.el
@@ -4,13 +4,21 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(TeX-view-program-selection
+   '(((output-dvi has-no-display-manager) "dvi2tty")
+     ((output-dvi style-pstricks) "dvips and gv") (output-dvi "xdvi")
+     (output-pdf "PDF Tools") (output-html "xdg-open")))
  '(custom-safe-themes
    '("0dd2666921bd4c651c7f8a724b3416e95228a13fca1aa27dc0022f4e023bf197"
      "b11edd2e0f97a0a7d5e66a9b82091b44431401ac394478beb44389cf54e6db28"
      "04aa1c3ccaee1cc2b93b246c6fbcd597f7e6832a97aaeac7e5891e6863236f9f"
      "6bdc4e5f585bb4a500ea38f563ecf126570b9ab3be0598bdf607034bb07a8875" default))
  '(org-confirm-babel-evaluate nil)
+<<<<<<< HEAD
  '(org-latex-packages-alist nil)
+=======
+ '(org-export-backends '(ascii beamer html icalendar latex man odt))
+>>>>>>> 30f811f (cleanup)
  '(org-latex-preview-cache 'temp)
  '(org-safe-remote-resources '("\\`https://fniessen\\.github\\.io\\(?:/\\|\\'\\)"))
  '(org-startup-with-link-previews t)
